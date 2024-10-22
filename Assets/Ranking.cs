0b58810802cb194ea3fe360ff10952025c6af008
Gabriel Pivoto <gabrielspivoto@gmail.com>
2024-10-21 23:22:38 -0300

    implementando sistemas de troca de telas

---
+++ Assets/Ranking.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ranking : MonoBehaviour
+{
+    public void BackToMenu(){
+        SceneManager.LoadSceneAsync(0);
+    }
+}
