bb1229e768966c59999647d6cff65183a4185ebf
silasjunior360 <142449030+silasjunior360@users.noreply.github.com>
2024-11-28 15:56:19 -0300

    final

--- Assets/Scripts/MainMenu/RankingController.cs
+++ Assets/Scripts/MainMenu/RankingController.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class RankingController : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class RankingController : MonoBehaviour{
+    public TMP_Text Posicao;
+    public TMP_Text Nome;
+    public TMP_Text Pontuacao;
+
+    private int posicao;
+    private string nome;
+    private int pontuacao;
+
     void Start()
     {
-        
+        posicao = 0; 
+        nome = "Player"; 
+        pontuacao = 0; 
+
+        AtualizarRank();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AtualizarRank()
     {
-        
+        Posicao.text = posicao.ToString();
+        Nome.text = nome;
+        Pontuacao.text = pontuacao.ToString();
+    }
+
+    public void ConfigurarRank(int novaPosicao, string novoNome, int novaPontuacao)
+    {
+        posicao = novaPosicao;
+        nome = novoNome;
+        pontuacao = novaPontuacao;
+
+        AtualizarRank();
     }
 }
